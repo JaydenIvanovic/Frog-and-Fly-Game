8ec49c1bf3cd91219f105a26fb270149d1a6b9d2
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-08-02 14:31:08 +1000

    Fixed sprite sheet (some problems with shadows of other sprites being too close). Layered the trees better. Made a visual boundary (blue quads for water). Changed my movement scripts over to Justin's as they were much better. Added a misc sprite sheet with gold (could seek towards it) and bones (flee from it). Now on to steering!

--- GAITAssignment/Assets/Scripts/Movement.cs
+++ GAITAssignment/Assets/Scripts/Movement.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Movement : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour 
+{
+	public float speed = 3f;
+	public float acceleration = 10f;
+	
+	public void Move(Vector3 velocity)
+	{
+		// Get the vectors direction.
+		if (velocity.sqrMagnitude > 1)
+			velocity = velocity.normalized;
+		
+		// Give it some speed.
+		velocity *= speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		var accel = (velocity - rigidbody.velocity) * acceleration;
+		// Square acceleration as we have the sqrMagnitude of accel.
+		if (accel.sqrMagnitude > acceleration * acceleration)
+			accel = accel.normalized * acceleration;
+
+		rigidbody.velocity += accel * Time.deltaTime;
 	}
 }
