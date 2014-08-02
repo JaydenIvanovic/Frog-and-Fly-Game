ebd54f23ea20664b4cd60e86fd07c7468ac6dddd
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-08-02 17:27:37 +1000

    Added in a few prefabs of basic prey for the player. Implemented the seek/flee steering behaviours. If you run the project at the moment and select scene view and zoom out, you will see the seek behaviour in action (seeks the chest) and how it oscillates. This is easily fixed (arrive behaviour). I'm going to think of a way to combine behaviours nicely.

---
+++ GAITAssignment/Assets/Scripts/Steering.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SteeringOutput
+{
+	public Vector3 linearVel;
+	public float angularVel;
+}
+
+public class Steering 
+{
+	private Vector3 character, target;
+	public float maxAcceleration = 5f;
+
+	public Steering(Vector3 character, Vector3 target)
+	{
+		this.character = character;
+		this.target = target;
+	}
+
+	public SteeringOutput getSteering()
+	{
+		var steering = new SteeringOutput();
+
+		steering.linearVel = target - character;
+		steering.linearVel.Normalize();
+		Debug.Log(steering.linearVel.ToString());
+		steering.linearVel *= maxAcceleration;
+
+		// The angular velocity will be handled by other
+		// behaviours such as 'align'.
+		steering.angularVel = 0;
+
+		return steering;
+	}
+
+	public void updatePlayerPosition(Vector3 position)
+	{
+		character = position;
+	}
+}
