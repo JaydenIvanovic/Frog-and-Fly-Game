fcba8d8c0628dfe497630ff8ba295eb80b59b81f
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-08-02 21:31:30 +1000

    Added a drop down box in the inspector view to select what behaviour you want the "prey" (red square) to perform. Renamed the Behaviour class to SteeringBehaviour as it clashed with a Unity specific class. Additionally, I refactored some code.

--- GAITAssignment/Assets/Scripts/Steering.cs
+++ GAITAssignment/Assets/Scripts/Steering.cs
@@ -22,14 +22,18 @@ public class Steering
 	{
 		var steering = new SteeringOutput();
 
+		// Get direction to target and make it a unit length
+		// vector before heading there at maximum speed.
 		steering.linearVel = target - character;
 		steering.linearVel.Normalize();
-		Debug.Log(steering.linearVel.ToString());
 		steering.linearVel *= maxAcceleration;
+
+		// As we are working in 2D, ensure z is 0
+		steering.linearVel.z = 0f;
 
 		// The angular velocity will be handled by other
 		// behaviours such as 'align'.
-		steering.angularVel = 0;
+		steering.angularVel = 0f;
 
 		return steering;
 	}
---
+++ GAITAssignment/Assets/Scripts/SteeringBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Mainly a helper to construct the desired behaviour.
+public enum BType 
+{
+	Seek, 
+	Flee
+}
+
+// The class which should be attached to a gameobject.
+// In inspector set the type of behaviour wanted as indicated
+// via the BType enum.
+[RequireComponent(typeof(Movement))]
+public class SteeringBehaviour : MonoBehaviour 
+{
+	public BType selectedBehaviour;
+	private Steering steering;
+	public GameObject targetObj;
+
+	// Use this for initialization
+	void Start () 
+	{
+		switch(selectedBehaviour)
+		{
+			case BType.Seek:
+				steering = new Steering(transform.position, targetObj.transform.position);
+				break;
+			case BType.Flee:
+				steering = new Steering(targetObj.transform.position, transform.position);
+				break;
+			default:
+				break;
+		}
+
+		SetDefaults();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		SteeringOutput so = steering.getSteering();
+
+		// Update the position and orientation of the gameobject.
+		transform.position += rigidbody.velocity * Time.deltaTime;
+		// Multiply as rotation is a quaternion
+		rigidbody.rotation *= Quaternion.Euler(rigidbody.angularVelocity * Time.deltaTime);
+
+		// Update the rigidbody velocities for next update.
+		rigidbody.velocity += so.linearVel * Time.deltaTime;
+		// Cos for z as we would rather rotate around the z-axis rather than the x-axis from our ortho perspective
+		rigidbody.angularVelocity += new Vector3(0f, Mathf.Sin(so.angularVel), Mathf.Cos(so.angularVel)) * Time.deltaTime;
+
+		// During "Seek" the character moves so we must update their position in the steering class.
+		if (selectedBehaviour == BType.Seek)
+			steering.updatePlayerPosition(transform.position);
+	}
+	
+	// For testing given an arbitrary initial velocity
+	private void SetDefaults()
+	{
+		rigidbody.velocity = Vector3.left * 4f; 
+		rigidbody.angularVelocity = Vector3.zero;
+	}
+}
