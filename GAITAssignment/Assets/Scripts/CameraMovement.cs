4ad5d04479b7521e8225cad9821770909c434e57
Michael Dann <s3365362@student.rmit.edu.au>
2014-08-07 17:40:58 +1000

    Changed camera movement so that it doesn't scroll to the "blue area"

--- GAITAssignment/Assets/Scripts/CameraMovement.cs
+++ GAITAssignment/Assets/Scripts/CameraMovement.cs
@@ -6,6 +6,11 @@ public class CameraMovement : MonoBehaviour {
 
 	public Transform _player;
 
+	public float AdditionalLeftBuffer;
+	public float AdditionalRightBuffer;
+	public float AdditionalTopBuffer;
+	public float AdditionalBottomBuffer;
+
 	// Use this for initialization
 	void Start () {
 
@@ -13,6 +18,26 @@ public class CameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(_player.position.x, _player.position.y, transform.position.z);
+
+		//Camera.main.aspect
+		float xBuffer, yBuffer;
+
+		if (Camera.main.aspect > 1.0f) {
+			xBuffer = Camera.main.orthographicSize * Camera.main.aspect;
+			yBuffer = Camera.main.orthographicSize;
+		} else {
+			xBuffer = Camera.main.orthographicSize;
+			yBuffer = Camera.main.orthographicSize * Camera.main.aspect;
+		}
+
+		float xPos = Mathf.Clamp (_player.position.x,
+		                          GameObject.Find ("LeftBoundary").transform.position.x + xBuffer - AdditionalLeftBuffer,
+		                          GameObject.Find ("RightBoundary").transform.position.x - xBuffer + AdditionalRightBuffer);
+
+		float yPos = Mathf.Clamp (_player.position.y,
+		                          GameObject.Find ("BottomBoundary").transform.position.y + yBuffer - AdditionalBottomBuffer,
+		                          GameObject.Find ("TopBoundary").transform.position.y - yBuffer + AdditionalTopBuffer);
+
+		transform.position = new Vector3(xPos, yPos, transform.position.z);
 	}
 }
