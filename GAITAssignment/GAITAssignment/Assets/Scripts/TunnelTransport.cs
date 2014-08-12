db2dd703eb271e9e639d16115c11756c374ca90a
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-08-12 22:27:26 +1000

    Player can run into the hideouts to travel between them like a tunnel system. This might be cool to have to get across the map quickly.

---
+++ GAITAssignment/GAITAssignment/Assets/Scripts/TunnelTransport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TunnelTransport : MonoBehaviour 
+{
+	public GameObject outTunnel; // The tunnel that this one leads to
+	
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Player")
+			coll.gameObject.transform.position = new Vector3(outTunnel.transform.position.x, outTunnel.transform.position.y - 1f, 0f);
+	}
+}
