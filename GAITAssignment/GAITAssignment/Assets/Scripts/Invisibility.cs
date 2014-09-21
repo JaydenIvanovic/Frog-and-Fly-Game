8756b653a8ba4965215656bec0d69852e4856d2e
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-09-21 23:30:01 +1000

    Started adding in the frogs. I thought it would make it more challenging if they were invisible until the flies are within a certain distance(kinda like fog of war), so I implemented the logic for doing that, but it may not be the best way to demo our frog AI. Perhaps one frog can always be seen?

---
+++ GAITAssignment/GAITAssignment/Assets/Scripts/Invisibility.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Invisibility : MonoBehaviour 
+{
+	public float visibleDistance;
+	public GameObject flyPlayer;
+	public GameObject smokePrefab;
+	private List<Transform> flies;
+	private SpriteRenderer frogRenderer;
+	private SpriteRenderer tongueRenderer;
+	private GameObject smokeInst;
+	private bool visible;
+
+	// Use this for initialization
+	void Start () 
+	{
+		flies = new List<Transform>();
+		foreach (Transform t in flyPlayer.GetComponentInChildren<Transform>()) {
+			flies.Add(t);
+		}
+
+		frogRenderer = GetComponent<SpriteRenderer>();
+
+		foreach (Transform t in GetComponentInChildren<Transform>()) {
+			if (t.name == "Tongue") {
+				tongueRenderer = t.gameObject.GetComponent<SpriteRenderer>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach (Transform fly in flies) {
+			if (Vector2.Distance(fly.position, transform.position) <= visibleDistance) {
+				frogRenderer.enabled = true;
+				tongueRenderer.enabled = true;
+
+				if(smokeInst == null)
+					smokeInst = (GameObject)Instantiate(smokePrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z - 1), Quaternion.identity);
+
+				return;
+			}
+		}
+
+		if(smokeInst)
+			Destroy(smokeInst);
+
+		frogRenderer.enabled = false;
+		tongueRenderer.enabled = false;
+	}
+}
