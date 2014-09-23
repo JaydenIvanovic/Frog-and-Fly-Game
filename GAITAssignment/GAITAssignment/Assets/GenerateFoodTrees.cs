89db2609d0ea01e42c2da633b2e61f0b2f66c823
JaydenIvanovic <s3331571@student.rmit.edu.au>
2014-09-23 19:02:25 +1000

    Started implementing a naive system to procedurally generate the position of food trees at the start of the game.

---
+++ GAITAssignment/GAITAssignment/Assets/GenerateFoodTrees.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateFoodTrees : MonoBehaviour 
+{
+	public int numAreas;
+	public int minGroupSize;
+	public float minxBoundary, maxxBoundary;
+	public float minyBoundary, maxyBoundary;
+	public GameObject appleTreePrefab, flowerTreePrefab;
+	public GameObject home;
+	public float requiredDistanceFromHome, distanceFromNeighbours;
+
+	// Randomly place the apple and flower trees on the map
+	// so the player doesn't have existing knowledge of there
+	// location.
+	void Start() 
+	{
+		for (int i = 0; i < numAreas; ++i) {
+			Vector3 lastTreePos;
+
+			for (int j = 0; j < minGroupSize; ++j) {
+				
+				Vector3 newTreePos;
+				if (!lastTreePos) {
+					// Generate and test approach. Find a random position and see if it meets our requirement.
+					do {
+						newTreePos = new Vector3(Random.Range(minxBoundary, maxxBoundary+1), Random.Range(minyBoundary, maxyBoundary+1), 1);
+					} while (Vector3.Distance(home.transform.position, newTreePos) < requiredDistanceFromHome);
+					Debug.Log(newTreePos);
+				} else {
+					// We want to find a tree close to the last tree.
+					newTreePos = new Vector3(Random.Range(lastTreePos.x - 2, lastTreePos.x + 2), Random.Range(lastTreePos.y - 2, lastTreePos.y + 2), 1);
+				}
+
+				// Randomly choose between an apple and flower tree.
+				if (Random.value < 0.5) {
+					GameObject tree = (GameObject)Instantiate(appleTreePrefab, newTreePos, Quaternion.identity);
+					tree.transform.parent = transform;
+				} else {
+					GameObject tree = (GameObject)Instantiate(flowerTreePrefab, newTreePos, Quaternion.identity);
+					tree.transform.parent = transform;
+				}
+
+				lastTreePos = newTreePos;
+			}
+
+		}
+	}
+
+}
